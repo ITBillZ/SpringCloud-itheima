81a3e7b859604cbf73b228367b4cd5380bbd9e71
Bill <704317542@qq.com>
2023-05-17 22:32:22 +0800

    update order-service and user-service

---
+++ order-service/src/main/java/cn/itbill/order/mapper/OrderMapper.java
@@ -0,0 +1,12 @@
+package cn.itbill.order.mapper;
+
+import cn.itbill.order.pojo.Order;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderMapper {
+
+    @Select("select * from tb_order where id = #{id}")
+    Order findById(Long id);
+}
---
+++ user-service/src/main/java/cn/itbill/user/service/UserService.java
@@ -0,0 +1,17 @@
+package cn.itbill.user.service;
+
+import cn.itbill.user.mapper.UserMapper;
+import cn.itbill.user.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public User queryById(Long id) {
+        return userMapper.findById(id);
+    }
+}
