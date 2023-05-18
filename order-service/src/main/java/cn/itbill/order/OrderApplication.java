a16b849c77671e145581cb60eedbd25228243cfc
Bill <704317542@qq.com>
2023-05-18 15:30:43 +0800

    update feign and fix bug

--- order-service/src/main/java/cn/itbill/order/OrderApplication.java
+++ order-service/src/main/java/cn/itbill/order/OrderApplication.java
@@ -1,14 +1,20 @@
 package cn.itbill.order;
 
+import cn.itbill.feign.clients.UserClient;
+import cn.itbill.user.config.PatternProperties;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @MapperScan("cn.itbill.order.mapper")
 @SpringBootApplication
+@EnableFeignClients(
+        clients = {UserClient.class},
+        defaultConfiguration = PatternProperties.class)
 public class OrderApplication {
 
     public static void main(String[] args) {
@@ -18,9 +24,9 @@ public class OrderApplication {
     /**
      * 创建RestTemplate并注入spring容器
      */
-    @Bean
-    @LoadBalanced  // 负载均衡
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+//    @Bean
+//    @LoadBalanced  // 负载均衡
+//    public RestTemplate restTemplate() {
+//        return new RestTemplate();
+//    }
 }
